ccbf4575383d70d921b06a4798de067451980535
Sachin Nimmalapudi <sachinn1087@gmail.com>
2024-01-29 14:48:15 -0500

    logging and exception

---
+++ src/pipeline/logger.py
@@ -0,0 +1,24 @@
+"""
+any execution that happens will be tracked and logged so we can track any errors such as custom exception errors
+"""
+import logging
+import os
+from datetime import datetime
+
+"""
+will create text files using the naming convention set.
+"""
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+log_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(log_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(log_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+if __name__=="__main__":
+    logging.info("Logging has started")
